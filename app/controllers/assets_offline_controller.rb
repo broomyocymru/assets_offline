900286f94424857ca056486bc6b8669c9712587f
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-01-13 16:06:25 -0500

    Moved main logic into view helper methods and updated manifest view to make use of these.

--- app/controllers/assets_offline_controller.rb
+++ app/controllers/assets_offline_controller.rb
@@ -4,8 +4,12 @@ class AssetsOfflineController < ActionController::Base
   Mime::Type.register 'text/cache-manifest', :manifest
 
   def show
+    # Load the configuration file and make it accessible to the template.
     config = YAML.load_file("#{Rails.root}/config/assets_offline.yml")
-    root = config[params[:id]]
+    @root = config[params[:id]]
+
+    # Generate the checksum for the file.
+    @key = generate_key()
 
     respond_to do |format|
       format.manifest
