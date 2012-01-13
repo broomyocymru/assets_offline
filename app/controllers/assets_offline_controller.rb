f9683a251208d2ad39d375340f872fb1eeeded8e
Jordan MacDonald <jordan.macdonald@greatersudbury.ca>
2012-01-13 15:12:34 -0500

    Updated the show action to respond with a new manifest mime type. Updated main controller to use manifest mime type as default.

--- app/controllers/assets_offline_controller.rb
+++ app/controllers/assets_offline_controller.rb
@@ -1,63 +1,15 @@
 require 'yaml'
 
 class AssetsOfflineController < ActionController::Base
+  Mime::Type.register 'text/cache-manifest', :manifest
 
   def show
     config = YAML.load_file("#{Rails.root}/config/assets_offline.yml")
     root = config[params[:id]]
 
-    body =  "# GENERATED USING ASSET_OFFLINE\n"
-    body <<  "# Mode: #{Rails.env} \n"
-    body << "# #{generate_key()} \n\n"
-
-    body << "CACHE MANIFEST \n"
-    # CACHE SECTION
-    if !root["asset-cache"].blank? and root["asset-cache"].count() > 0
-      root["asset-cache"].each do |asset_name|
-        body << "#{view_context.asset_path(asset_name)}\n"
-      end
+    respond_to do |format|
+      format.manifest
     end
-    if !root["cache"].blank? and root["cache"].count() > 0
-      root["cache"].each do |asset_name|
-        body << "#{asset_name}\n"
-      end
-    end
-    body << "\n"
-
-    # NETWORK SECTION
-    body << "NETWORK:\n"
-    body << "# Resources where a connection is required \n"
-    if !root["asset-network"].blank? and root["asset-network"].count() > 0
-      root["asset-network"].each do |asset_name|
-        body << "#{view_context.asset_path(asset_name)}\n"
-      end
-    end
-    if !root["network"].blank? and root["network"].count() > 0
-      root["network"].each do |asset_name|
-        body << "#{asset_name}\n"
-      end
-    end
-    body << "\n"
-
-    #FALLBACK SECTION
-    body << "FALLBACK:\n"
-    body << "# Resources where a connection is used if available \n"
-    if !root["asset-fallback"].blank? and root["asset-fallback"].count() > 0
-      root["asset-fallback"].each do |asset_name|
-        asset_name_part = asset_name.split("#")
-        body << "#{view_context.asset_path(asset_name_part[0].strip())} #{view_context.asset_path(asset_name_part[1].strip())}\n"
-      end
-    end
-    if !root["fallback"].blank? and root["fallback"].count() > 0
-      root["fallback"].each do |asset_name|
-        asset_name_part = asset_name.split("#")
-        body << "#{asset_name_part[0].strip()} #{asset_name_part[1].strip()}\n"
-      end
-    end
-    body << "\n"
-
-    headers['Content-Type'] = 'text/cache-manifest'
-    render :text => body, :layout => false
   end
 
   private
